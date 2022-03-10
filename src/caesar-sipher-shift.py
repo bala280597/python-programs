075e9c85d52c0249a4a1cf6df8a1ed69a7ee9500
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-03-10 14:43:54 +0530

    Update caesar-sipher-shift.py

--- src/caesar-sipher-shift.py
+++ src/caesar-sipher-shift.py
@@ -1,47 +1,27 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(text,shift):
-  
+def caesar(text,shift,direction):
   secret = []
   text=list(text)
   for i in range(0,len(text)):
-    for j in range(0,len(alphabet)):
-      if(text[i]==alphabet[j]):
-        j+=shift
-        if(j > len(alphabet)):
-          j-=len(alphabet)
-        secret.append(alphabet[j])
-        break
+    position= alphabet.index(text[i])
+    if(direction == "encode"):
+      position+=shift
+      if(position > len(alphabet)):
+        position-=len(alphabet)
+    if(direction == "decode"):
+      position-=shift
+      if(position > len(alphabet)):
+        position+=len(alphabet)
+    secret.append(alphabet[position])
   secret= ''.join(map(str,secret))
   return secret
-
-def decrypt(text,shift):
-  
-  secret = []
-  text=list(text)
-  for i in range(0,len(text)):
-    for j in range(0,len(alphabet)):
-      if(text[i]==alphabet[j]):
-        j-=shift
-        if(j < 0):
-          j+=len(alphabet)
-        secret.append(alphabet[j])
-        break
-  secret= ''.join(map(str,secret))
-  return secret
-
 
 repeat="yes"
 while repeat == "yes":
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  if(direction == "encode"):
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    cipher_encode=encrypt(text,shift)
-    print(f"The encrypted text is {cipher_encode}")
-  else:
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    cipher_decode = decrypt(text,shift)
-    print(f"The encrypted text is {cipher_decode}")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  result=caesar(text,shift,direction)
+  print(f"The {direction}ed text is {result}")
   repeat = input("Do you want to continue : yes or no")
