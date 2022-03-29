9b2bc6e1ea9fc1303b84fba4419c150bd0407d19
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-03-29 17:13:12 +0530

    Update turtle.py

--- src/turtle.py
+++ src/turtle.py
@@ -19,12 +19,13 @@ def square():
 
 
 def drawdot(space, x,tur):
+
     for i in range(x):
         for j in range(x):
+            r, g, b = random_colour()
+            turtle.pencolor((r, g, b))
             tur.dot()
 
-            tur.forward(space)
-        tur.backward(space * x)
 
         tur.right(90)
         tur.forward(space)
@@ -76,7 +77,7 @@ def spirograph(turtle):
     turtle.setheading(h + 5)
 
 
-# drawdot(10, 8,bala_the_turtle)
+#drawdot(15,15,bala_the_turtle)
 #dash(bala_the_turtle)
 # for i in range(3,11):
 #     bala_the_turtle.pensize(15)
@@ -85,6 +86,30 @@ def spirograph(turtle):
 #     draw(i)
 #random_walk(bala_the_turtle)
 # spirograph(bala_the_turtle)
+
+turtle.colormode(255)
+tim = turtle.Turtle()
+tim.ht()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
 
 screen = Screen()
 screen.exitonclick()
