9e4229b32a606caa8302ca1515acec87c04f8e19
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-04-01 11:39:04 +0530

    Update turtle.py

--- src/turtle.py
+++ src/turtle.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import Turtle, Screen
 import random
 bala_the_turtle = Turtle()
-bala_the_turtle.shape("turtle")
+bala_the_turtle.hideturtle()
 turtle.colormode(255)
 
 def random_colour():
@@ -76,6 +76,26 @@ def spirograph(turtle):
     h = turtle.heading()
     turtle.setheading(h + 5)
 
+def hist_paint(tim):
+    tim.ht()
+    tim.speed("fastest")
+    tim.penup()
+    tim.hideturtle()
+    tim.setheading(225)
+    tim.forward(300)
+    tim.setheading(0)
+    number_of_dots = 100
+    for dot_count in range(1, number_of_dots + 1):
+        r, g, b = random_colour()
+        tim.dot(20, (r,g,b))
+        tim.forward(50)
+        if dot_count % 10 == 0:
+            tim.setheading(90)
+            tim.forward(50)
+            tim.setheading(180)
+            tim.forward(500)
+            tim.setheading(0)
+
 
 #drawdot(15,15,bala_the_turtle)
 #dash(bala_the_turtle)
@@ -87,29 +107,7 @@ def spirograph(turtle):
 #random_walk(bala_the_turtle)
 # spirograph(bala_the_turtle)
 
-turtle.colormode(255)
-tim = turtle.Turtle()
-tim.ht()
-tim.speed("fastest")
-tim.penup()
-tim.hideturtle()
-color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
-tim.setheading(225)
-tim.forward(300)
-tim.setheading(0)
-number_of_dots = 100
 
-for dot_count in range(1, number_of_dots + 1):
-    tim.dot(20, random.choice(color_list))
-    tim.forward(50)
-
-    if dot_count % 10 == 0:
-        tim.setheading(90)
-        tim.forward(50)
-        tim.setheading(180)
-        tim.forward(500)
-        tim.setheading(0)
-
-
+hist_paint(bala_the_turtle)
 screen = Screen()
 screen.exitonclick()
