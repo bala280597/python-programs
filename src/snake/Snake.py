4c2d98222b0f081733a668bbef942120343b2ccf
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-04-18 13:18:34 +0530

    Create Snake.py

---
+++ src/snake/Snake.py
@@ -0,0 +1,42 @@
+from turtle import Screen,Turtle
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake():
+    def __init__(self):
+        self.segments = []
+        self.create()
+        self.head = self.segments[0]
+
+
+    def create(self):
+        position = [(0, 0), (20, 0), (40, 0)]
+        for i in position:
+            obj = Turtle()
+            obj.penup()
+            obj.goto(i)
+            obj.shape("square")
+            obj.color("red")
+            self.segments.append(obj)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+          self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+          self.head.setheading(DOWN)
+    def left(self):
+        if self.head.heading() != RIGHT:
+          self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+          self.head.setheading(RIGHT)
