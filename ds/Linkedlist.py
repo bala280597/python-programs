3a75b31987451b8aa7886b5af7320cfa9144e67c
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-07-05 22:53:12 +0530

    Update Linkedlist.py

--- ds/Linkedlist.py
+++ ds/Linkedlist.py
@@ -27,6 +27,29 @@ class Linkedlist:
         self.length += 1
         return True
 
+    def get(self,index):
+        temp = self.head
+        if self.length == 0:
+            return self.head.value
+        elif index < 0 or index >= self.length :
+            return None
+        elif temp.next == None:
+            return self.tail.value
+        else:
+          for i in range(index):
+              temp = temp.next
+          return temp
+
+    def set_value(self, index, value):
+
+        if index < 0 or index >= self.length:
+            return None
+        else:
+            temp = self.get(index)
+            temp.value = value
+            return True
+        return False
+
     def pop(self):
         if self.length == 0:
             return None
@@ -43,12 +66,76 @@ class Linkedlist:
             self.tail = None
         return temp.value
 
+    def prepend_pop(self):
+        if self.length == 0:
+            return None
+        else:
+          temp = self.head
+          new = temp.next
+          self.head = new
+          self.length -= 1
+          return temp.value
+
+    def prepend_add(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+          temp = self.head
+          new_node.next = temp
+          self.head = new_node
+          self.length += 1
+
+    def insert_at_point(self,index,value):
+        if index < 0 or index >= self.length:
+            return True
+        if index == 0:
+            return self.prepend_add(value)
+        elif index == self.length:
+            return self.append_new_node(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index-1)
+            next_value = self.get(index)
+            temp.next = new_node
+            new_node.next = next_value
+            self.length += 1
+            return False
+
+    def delete_at_point(self,index):
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            self.prepend_pop()
+        elif index == self.length:
+            self.pop()
+        else:
+            new_node = self.get(index-1)
+            next_value = self.get(index)
+            new_node.next = next_value.next
+            self.length -= 1
 
 
-my_linked_list = Linkedlist(4)
+my_linked_list = Linkedlist(15)
 my_linked_list.append_new_node(5)
 my_linked_list.append_new_node(6)
-print(my_linked_list.pop())
+my_linked_list.print_list()
+print('-----------')
+my_linked_list.insert_at_point(1,44)
+my_linked_list.print_list()
+print('--------')
+my_linked_list.delete_at_point(1)
 my_linked_list.print_list()
 
+
+#print(my_linked_list.pop())
+# print(my_linked_list.prepend_pop())
+# print(my_linked_list.prepend_pop())
+# print(my_linked_list.prepend_pop())
+#my_linked_list.prepend_add(10)
+#my_linked_list.print_list()
+#print(my_linked_list.get(2))
+#my_linked_list.set_value(0,20)
+#my_linked_list.print_list()
 #print(my_linked_list.tail.value)
